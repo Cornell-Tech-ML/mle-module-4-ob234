674bdaedcd4dc89cf98e33183ec98d0b16dac85d
Olena <ob234@cornell.edu>
2023-12-05 22:36:59 -0500

    module style fixes

--- minitorch/module.py
+++ minitorch/module.py
@@ -38,34 +38,33 @@ class Module:
 
     def eval(self) -> None:
         "Set the mode of this module and all descendent modules to `eval`."
-        #ASSIGN 0.4
+        # ASSIGN 0.4
         for m in self.modules():
-          m.eval()
-        self.training = False 
-        #END ASSIGN 0.4
+            m.eval()
+        self.training = False
+        # END ASSIGN 0.4
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """
         Collect all the parameters of this module and its descendents.
         Returns: The name and `Parameter` of each ancestor parameter.
         """
-        #ASSIGN0.4
-        parameters = {} 
+        # ASSIGN0.4
+        parameters = {}
         for k , v in self._parameters.items():
-          parameters[k] = v
-        
+            parameters[k] = v
         for mod_name, m in self._modules.items():
-          for k, v in m.named_parameters():
-            parameters[f"{mod_name}.{k}"] = v 
+            for k, v in m.named_parameters():
+                parameters[f"{mod_name}.{k}"] = v
         return list(parameters.items())
-        #END ASSIGN0.4 
-
+        # END ASSIGN0.4
+        
     def parameters(self) -> Sequence[Parameter]:
         "Enumerate over all the parameters of this module and its descendents."
-        #ASSIGN0.4
+        # ASSIGN0.4
         return [j for _, j in self.named_parameters()]
-        #END ASSIGN0.4
-
+        # END ASSIGN0.4
+        
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """
         Manually add a parameter. Useful helper for scalar parameters.
@@ -156,3 +155,4 @@ class Parameter:
 
     def __str__(self) -> str:
         return str(self.value)
+    
